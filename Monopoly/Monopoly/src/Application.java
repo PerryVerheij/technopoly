f3fd6bb2ed5deaaf2a106f6cbd59429b174c6ab7
Hger4 <huizinggerard@gmail.com>
2021-11-30 12:37:22 +0100

    Update Application.java

--- Monopoly/Monopoly/src/Application.java
+++ Monopoly/Monopoly/src/Application.java
@@ -7,27 +7,7 @@ public class Application implements Runnable {
     }
 
     public void run() {
-        Person newPerson = new Person();
 
-        newPerson.firstName = "Perry";
-        newPerson.lastName = "Verheij";
-        newPerson.city = "Ede";
-
-        printPerson(newPerson);
-
-        newPerson.firstName = "Joyce";
-        newPerson.lastName = "Tempelman";
-        newPerson.city = "Ede";
-
-        printPerson(newPerson);
-
-        newPerson.firstName = "Ardguan";
-        newPerson.lastName = "Boenjunjn";
-        newPerson.city = "Ede";
-
-        printPerson(newPerson);
     }
-    public void printPerson(Person somePerson) {
-        SaxionApp.printLine(somePerson.firstName + " " + somePerson.lastName + " woont in " + somePerson.city);
-    }
+
 }
